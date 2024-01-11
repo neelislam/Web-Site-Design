5215e15acce7f1a3bfe6999d199070aaeb6f818f
Rabiul Islam <65666738+neelislam@users.noreply.github.com>
2024-01-11 23:03:02 +0600

    Transparent Login Page

---
+++ light.js
@@ -0,0 +1,10 @@
+function getRandomColor() {
+    let val1 = Math.ceil(0 + Math.random() * 255);
+    let val2 = Math.ceil(0 + Math.random() * 255);
+    let val3 = Math.ceil(0 + Math.random() * 255);
+    return `rgb(${val1},${val2},${val3})`;
+}
+
+setInterval(() => {
+    document.querySelector(".login").style.background = getRandomColor();
+}, 900);
